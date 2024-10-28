88213f8d42698a8f6854743844598c0372d5eea6
hcastanodeloitte <hcastano@deloitte.com.au>
2024-10-28 18:18:01 +1100

    Clean duplicate files. only web cmp left

--- views/script.js
+++
@@ -1,9 +0,0 @@
-const kjInput = document.getElementById('kjInput');
-const convertButton = document.getElementById('convertButton');
-const result = document.getElementById('result');
-
-convertButton.addEventListener('click', () => {
-  const kjValue = parseFloat(kjInput.value);
-  const calories = kjValue / 4.184;
-  result.textContent = `${kjValue} kJ is approximately ${calories.toFixed(2)} calories.`;
-});
